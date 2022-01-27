1664dbed75499b4cecbdb2961f2254acc59c1a65
Oleksii Skryba <Oleksii.Skryba@Gmail.Com>
2022-01-27 19:12:56 +0200

    add hw03

---
+++ hw0/hw02/main.js
@@ -0,0 +1,62 @@
+let arrNum = [1, 2, 3, 4, 5];
+let arrStr = ["one", "to", "three", "four", "five"];
+let arrNumStrBool = [1, 2, 3, 4, 5, "one", "to", "three", "four", "five", true, false];
+// console.log(arrNamStrBool);
+
+// let arrX =[];
+// arrX[0] = 1;
+// arrX[1] = 2;
+// arrX[2] = 3;
+
+// console.log(arrX);
+
+// for (let i = 0; i < 10; i++) {
+//     document.write(`<div>* ${i} </div>`);
+// };
+
+// let i=0
+// while (i<20) {
+//     document.write(`<h1>* ${++i}</h1>`)
+//     // i++;
+// };
+
+// for (const i of arrNumStrBool) {
+//     if (typeof(i)=="boolean") {
+//       console.log(i);  
+//     }
+// }
+
+// for (let m = 0; m < 2; m++) {
+//     for (let s = 0; s < 60; s++) {
+//         console.log(m, s);
+//     }
+// };
+
+// for (let h = 0; h <= 2; h++) {
+//     let min; h==2?min=20:min=60;
+//     for (let m = 0; m < min; m++) {
+//     for (let s = 0; s < 60; s++) {
+//         console.log(h, m, s);
+//     }
+// };
+// };
+
+// let arrN = [];
+// for (let i = 0; i < arrNum.length; i++) {
+//     arrN.unshift(arrNum[i]);
+// };
+// console.log(arrN);
+
+// console.log(arrNum);
+
+// while (arrStr.length) {
+//       arrNum.splice(3, 0, arrStr.pop());  
+// }
+
+// console.log(arrNum);
+
+// for (const i of arrNum) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// };
