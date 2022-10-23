82c6624a83bd6e9ae9232ee64c4c5e1646f12f05
Oleksii Skryba <Oleksii.Skryba@Gmail.Com>
2022-10-23 20:00:08 +0200

    js0

---
+++ HW/hw00/main.js
@@ -0,0 +1,41 @@
+let str0 = 'hello';
+let str1 = 'owu';
+let str2 = 'com';
+let str3 = 'ua';
+let num0 = 1;
+let num1 = 10;
+let num2 = -999;
+let num3 = 123;
+let num4 = 3.14;
+let num5 = 2.7;
+let num6 = 16;
+let isFolse = true;
+let isTrue = false;
+
+// alert(str3);
+// document.write(str2);
+
+str0 = "Hello";
+console.log(str0+' '+str1);
+
+// let firstName = prompt('введите имя');
+// let lasttName = prompt('введите фамилию');
+// let age = +prompt('введите возраст');
+
+// console.log("Вітаю"+' '+ firstName+' '+ lasttName+' '+'. Тобі'+' '+ age+ ' років.');
+
+// console.log(typeof(str0));
+// console.log(typeof(num0));
+// console.log(typeof(isFolse));
+
+// console.log(5 < 6);  // true
+// console.log(5 > 6);  // false
+// console.log(5 == 6);  // false
+// console.log(5 === 6);  // false
+// console.log(10 == 10);  // true
+// console.log(10 === 10);  // true
+// console.log(10 != 10);  // false
+// console.log( 10 !== 10); // false
+// console.log( 10 === '10' );// false
+// console.log(123 === '123');  // false
+// console.log(123 == '123');  // true
--- HW/hw5/main2.js
+++
@@ -1,6 +0,0 @@
-
-let storeName = JSON.parse(localStorage.getItem('name'));
-// console.log(storeName);
-let h = document.createElement("h1");
-h.innerText = `Hello - ${storeName}`;
-document.body.appendChild(h);
