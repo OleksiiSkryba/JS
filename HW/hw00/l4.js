4cae1553a0a439a93ade3a9a9867313ae81d214b
Oleksii Skryba <Oleksii.Skryba@Gmail.Com>
2024-02-04 00:42:45 +0100

    old

--- HW/hw00/l4.js
+++ HW/hw00/l4.js
@@ -7,30 +7,80 @@
 //    this.phone = phone;
 //    this.website = website;
 //    this.company = company;
-
 // }
 
-// let i = new Human(1, 'Vasia', 'Pupkin', 'vasia@gmail.com',
-//  { 
-// street: 'Kulas Light',
-// suite: 'Apt. 556',
-// city: 'Gwenborough',
-// zipcode: '92998-3874',
-// geo: {
-//     lat: '-37.3159',
-//     lng: '81.1496'}},
-//     '1-770-736-8031 x56442',
-//     'hildegard.org',
-//     {
-//         name: 'Romaguera-Crona',
-//         catchPhrase: 'Multi-layered client-server neural-net',
-//         bs: 'harness real-time e-markets'
-//     }
-//     )
+class Human {
+    constructor(
+        id,name, username, email, address = {}, phone, website, company = {})
+   { this.id = id;
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.address = address;
+    this.phone = phone;
+    this.website = website;
+    this.company = company;
+   }
+   eat() {console.log('nom nom nom')};
+ }
 
-//     console.log(i);
-//     console.log(i.company.name);
+class Dav extends Human {
+    constructor(
+        id,name, username, email, address = {}, phone, website, company = {}, skils = [])
+        {
+        super(
+        id,name, username, email, address, phone, website, company)
+            this.skils = skils
+        }
+      coding(){console.log('tap tap tap')};
+    }
 
+let i = new Human(1, 'Vasia', 'Pupkin', 'vasia@gmail.com',
+ { 
+street: 'Kulas Light',
+suite: 'Apt. 556',
+city: 'Gwenborough',
+zipcode: '92998-3874',
+geo: {
+    lat: '-37.3159',
+    lng: '81.1496'}},
+    '1-770-736-8031 x56442',
+    'hildegard.org',
+    {
+        name: 'Romaguera-Crona',
+        catchPhrase: 'Multi-layered client-server neural-net',
+        bs: 'harness real-time e-markets'
+    }
+    )
+
+    console.log(i);
+    console.log(i.company.name);
+    i.eat();
+    // i.coding();
+
+let b = new Dav(1, 'Vasia', 'Pupkin', 'vasia@gmail.com',
+ { 
+street: 'Kulas Light',
+suite: 'Apt. 556',
+city: 'Gwenborough',
+zipcode: '92998-3874',
+geo: {
+    lat: '-37.3159',
+    lng: '81.1496'}},
+    '1-770-736-8031 x56442',
+    'hildegard.org',
+    {
+        name: 'Romaguera-Crona',
+        catchPhrase: 'Multi-layered client-server neural-net',
+        bs: 'harness real-time e-markets'
+    },
+    ['html', 'css', 'js']
+    );
+
+    console.log(b);
+    console.log(b.company.name);
+    b.eat();
+    b.coding();
 
 // class Tag{
 //     constructor(
@@ -51,33 +101,33 @@
 // console.log(a);
 
 
-class Client{
-    constructor(
-        id, name, surname , email, phone, order =[]
-    ){
-        this.id=id;
-        this.name=name;
-        this.surname=surname;
-        this.email=email;
-        this.phone=phone;
-        this.order=order;
-    }
-};
+// class Client{
+//     constructor(
+//         id, name, surname , email, phone, order =[]
+//     ){
+//         this.id=id;
+//         this.name=name;
+//         this.surname=surname;
+//         this.email=email;
+//         this.phone=phone;
+//         this.order=order;
+//     }
+// };
 
-let clients = [];
-const c1 = new Client(01,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04]);
-const c2 = new Client(02,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
-const c3 = new Client(03,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04, 05]);
-const c4 = new Client(04,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
-const c5 = new Client(05,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04, 05, 06]);
-const c6 = new Client(06,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
-const c7 = new Client(07,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04, 05]);
-const c8 = new Client(08,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
-const c9 = new Client(09,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04]);
-const c10 = new Client(10,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
-clients.push(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10);
-console.log(clients);
+// let clients = [];
+// const c1 = new Client(01,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04]);
+// const c2 = new Client(02,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
+// const c3 = new Client(03,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04, 05]);
+// const c4 = new Client(04,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
+// const c5 = new Client(05,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04, 05, 06]);
+// const c6 = new Client(06,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
+// const c7 = new Client(07,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04, 05]);
+// const c8 = new Client(08,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
+// const c9 = new Client(09,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03, 04]);
+// const c10 = new Client(10,' Vasia', 'Pupkin', 'a@blur.call', '+380111111', [01, 02, 03]);
+// clients.push(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10);
+// console.log(clients);
 
-sorts = clients.sort((a,b) => a.order.length-b.order.length);
-console.log(sorts);
+// sorts = clients.sort((a,b) => a.order.length-b.order.length);
+// console.log(sorts);
 
